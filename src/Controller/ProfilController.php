48f3f63e845598d6aac993c52aee7d0d1727c5b3
chlorella84 <dominika.sliwa@gmail.com>
2020-07-07 16:43:24 +0200

    debut formulaire connexion 2

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Participant;
+use App\Form\RegistrationFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProfilController extends AbstractController
 {
@@ -13,10 +18,31 @@ class ProfilController extends AbstractController
     /**
      * @Route("/profil/{id}", name="profil_mon_profil")
      */
-    public function monprofil()
+    public function monprofil(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        $user = new Participant();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('profil/profil.html.twig', [
-            'controller_name' => 'ProfilController',
+            'registrationForm' => $form->createView(),
         ]);
     }
 
