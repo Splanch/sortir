5225bd48d8aba9d9f6c7480e26c3dc34e7d09222
jean <jean.rupin@gmail.com>
2020-07-22 12:17:23 +0200

    Correction déclenchement actions par URL

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -20,55 +20,58 @@ class ProfilController extends AbstractController
     public function monProfil($id, $affichage = false, Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $repo = $this->getDoctrine()->getRepository(Participant::class);
-        $user = $repo->find($id);
-        if (!$user) {
-            return $this->render('security/login.html.twig');
-        } else {
-            $form = $this->createForm(RegistrationFormType::class, $user, ['userConnecte' => $this->getUser()]);
-            $form->handleRequest($request);
+        $userConnecte = $this->getUser();
+        $userProfil=$repo->find($id);
 
-            if ($form->isSubmitted() && $form->isValid()) {
-                // encode the plain password
-                $user->setPassword(
-                    $passwordEncoder->encodePassword(
-                        $user,
-                        $form->get('password')->getData()
-                    )
-                );
+        if ($userConnecte!=$userProfil) {
+            return $this->redirectToRoute('sortie_recherche');
+        }else {
+                $form = $this->createForm(RegistrationFormType::class, $userProfil, ['userConnecte' => $this->getUser()]);
+                $form->handleRequest($request);
 
-                if ($form->getData()->getAdministrateur()) {
-                    $user->setRoles(array('ROLE_ADMIN'));
-                } else {
-                    $user->setRoles(array('ROLE_USER'));
+                if ($form->isSubmitted() && $form->isValid()) {
+                    // encode the plain password
+                    $userConnecte->setPassword(
+                        $passwordEncoder->encodePassword(
+                            $userProfil,
+                            $form->get('password')->getData()
+                        )
+                    );
+
+                    if ($form->getData()->getAdministrateur()) {
+                        $userProfil->setRoles(array('ROLE_ADMIN'));
+                    } else {
+                        $userProfil->setRoles(array('ROLE_USER'));
+                    }
+
+                    $entityManager = $this->getDoctrine()->getManager();
+                    $entityManager->persist($userProfil);
+                    $entityManager->flush();
+                    $this->addFlash('success', 'Votre profil a été modifié.');
+                    return $this->redirectToRoute('sortie_recherche');
                 }
-
-                $entityManager = $this->getDoctrine()->getManager();
-                $entityManager->persist($user);
-                $entityManager->flush();
-                $this->addFlash('success', 'Votre profil a été modifié.');
-                return $this->redirectToRoute('sortie_recherche');
             }
+            return $this->render('profil/profil.html.twig', [
+                'registrationForm' => $form->createView(),
+                'affichage' => $affichage,
+            ]);
         }
-        return $this->render('profil/profil.html.twig', [
-            'registrationForm' => $form->createView(),
-            'affichage' => $affichage,
-        ]);
+
+        /**
+         * @Route("/profil/detail/{id}", name="profil_afficher")
+         */
+        public
+        function afficherprofil($id, $affichage = true, Request $request)
+        {
+            $repo = $this->getDoctrine()->getRepository(Participant::class);
+            $user = $repo->find($id);
+
+            return $this->render('profil/profil.html.twig', [
+                'user' => $user,
+                'affichage' => $affichage,
+            ]);
+
+        }
+
+
     }
-
-    /**
-     * @Route("/profil/detail/{id}", name="profil_afficher")
-     */
-    public function afficherprofil($id, $affichage = true, Request $request)
-    {
-        $repo = $this->getDoctrine()->getRepository(Participant::class);
-        $user = $repo->find($id);
-
-        return $this->render('profil/profil.html.twig', [
-            'user' => $user,
-            'affichage' => $affichage,
-        ]);
-
-    }
-
-
-}
